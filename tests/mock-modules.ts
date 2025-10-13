144d9077f7e066134598c51c98ba01a155eeac5f
Leonardo Gomes <lgdevlop@gmail.com>
2025-10-13 00:27:05 -0300

    ✨ feat(tests): add centralized mockModule utility function for test mocking

---
+++ tests/mock-modules.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prefer-const */
+import { mock } from "bun:test";
+
+/**
+ *
+ * @param modulePath - the path starting from this files' path.
+ * @param renderMocks - function to generate mocks (by their named or default exports)
+ * @returns an object
+ */
+export const mockModule = async (
+  modulePath: string,
+  renderMocks: () => Record<string, unknown>,
+) => {
+  let original = {
+    ...(await import(modulePath)),
+  };
+  let mocks = renderMocks();
+  let result = {
+    ...original,
+    ...mocks,
+  };
+  mock.module(modulePath, () => result);
+  return {
+    [Symbol.dispose]: () => {
+      mock.module(modulePath, () => original);
+    },
+  };
+};
